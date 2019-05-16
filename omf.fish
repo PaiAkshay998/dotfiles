7f29d4503bf92a8bb942c04ca827b674f2c8453b
Akshay Pai <paiakshay998@gmail.com>
2019-05-16 08:12:06 +0530

    Update omf

--- omf.fish
+++ omf.fish
@@ -39,18 +39,26 @@ alias wtf "alsabat"
 alias wtfbro "alsaloop"
 alias watchthis "./node_modules/.bin/webpack --watch"
 alias labssh "python2 ~/ssh_checker.py"
-alias androidstudio "/home/pai/android-studio/bin/studio.sh"
+alias androidstudio "/home/akshaypai/android-studio/bin/studio.sh"
 alias hpconnect "nmcli device wifi connect HP"
 alias gcc-4.4 "gcc34"
 alias g++-4.4 "g++34"
 alias do "ssh root@139.59.23.206"
+alias notif "i3-nagbar -m Notif"
+alias gnome-control-center "env XDG_CURRENT_DESKTOP=GNOME gnome-control-center"
+alias pyenv "source env/bin/activate.fish"
 set -gx GOPATH $HOME/go
-set -gx PYPATH /home/pai/.local/bin
+set -gx PYPATH /home/akshaypai/.local/bin
 set -gx FLUTTER $HOME/flutter/bin
-set -gx ANDROID_HOME $HOME/Android/Sdk
-
-set -gx PATH /usr/local/go/bin $GOPATH/bin $PATH $PYPATH $FLUTTER $ANDROID_HOME
+set -gx ANDROID_HOME $HOME/Android/android-studio/bin
+set -gx NPM_GLOBAL $HOME/.npm-global/bin
+set -gx TELEGRAM $HOME/backends/Telegram
+set -gx BOOST_ROOT /usr/local/boost_1_69_0
+set -gx PATH /usr/local/go/bin $GOPATH/bin $PATH $PYPATH $FLUTTER $ANDROID_HOME $NPM_GLOBAL $TELEGRAM $BOOST_ROOT
 
 set -gx DALAL_ENV "dev"
 set -gx SECRET_KEY "secret"
 set -gx DJANGO_SECRET_KEY "secret"
+set -gx PYCONF "/usr/include/python3.7m"
+set -gx CPLUS_INCLUDE_PATH $CPLUS_INCLUDE_PATH $PYCONF
+
